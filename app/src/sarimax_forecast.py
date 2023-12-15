4669ebb9d9768959deb7f42c69785ff287f84e65
yuenhern.yu@gmail.com <yuenhern.yu@gmail.com>
2023-12-15 22:52:42 +0800

    Wrap lines

--- app/src/sarimax_forecast.py
+++ app/src/sarimax_forecast.py
@@ -25,21 +25,45 @@ def _forecast_price_psf(data, order, in_sample_forecast_length=12, out_sample_fo
     return model_forecast['mean'], model_forecast['mean_ci_lower'], model_forecast['mean_ci_upper']
 
 
-def _plot_price_psf_forecast(mean_monthly_price_psf, mean_forecast, mean_ci_lower, mean_ci_upper, median_monthly_price_psf, median_forecast, median_ci_lower, median_ci_upper):
+def _plot_price_psf_forecast(
+    mean_monthly_price_psf, mean_forecast, mean_ci_lower, mean_ci_upper, 
+    median_monthly_price_psf, median_forecast, median_ci_lower, median_ci_upper
+):
     # Create Plotly figure with subplots
-    fig = make_subplots(rows=1, cols=1, subplot_titles=["Mean and Median Monthly Price PSF with Prediction Intervals in Kuala Lumpur"])
+    fig = make_subplots(
+        rows=1, cols=1, 
+        subplot_titles=["Mean and Median Monthly Price PSF with Prediction Intervals in Kuala Lumpur"]
+    )
 
     # Plot first time series and prediction interval
-    fig.add_trace(go.Scatter(x=mean_monthly_price_psf.index, y=mean_monthly_price_psf, mode='lines', name='Mean Price PSF'))
-    fig.add_trace(go.Scatter(x=mean_forecast.index, y=mean_forecast, mode='lines', name='Forecasted Mean'))
-    fig.add_trace(go.Scatter(x=mean_forecast.index, y=mean_ci_lower, fill=None, mode='lines', line=dict(color='rgba(0,0,255,0)')))
-    fig.add_trace(go.Scatter(x=mean_forecast.index, y=mean_ci_upper, fill='tonexty', mode='lines', line=dict(color='rgba(0,0,255,0.2)'), name=' Mean Price PSF Prediction Interval'))
+    fig.add_trace(go.Scatter(
+        x=mean_monthly_price_psf.index, y=mean_monthly_price_psf, mode='lines', name='Mean Price PSF'
+    ))
+    fig.add_trace(go.Scatter(
+        x=mean_forecast.index, y=mean_forecast, mode='lines', name='Forecasted Mean'
+    ))
+    fig.add_trace(go.Scatter(
+        x=mean_forecast.index, y=mean_ci_lower, fill=None, mode='lines', line=dict(color='rgba(0,0,255,0)')
+    ))
+    fig.add_trace(go.Scatter(
+        x=mean_forecast.index, y=mean_ci_upper, fill='tonexty', mode='lines', line=dict(color='rgba(0,0,255,0.2)'), 
+        name=' Mean Price PSF Prediction Interval'
+    ))
 
     # Plot second time series and prediction interval
-    fig.add_trace(go.Scatter(x=median_monthly_price_psf.index, y=median_monthly_price_psf, mode='lines', name='Median Price PSF'))
-    fig.add_trace(go.Scatter(x=median_forecast.index, y=median_forecast, mode='lines', name='Forecasted Median'))
-    fig.add_trace(go.Scatter(x=median_forecast.index, y=median_ci_lower, fill=None, mode='lines', line=dict(color='rgba(255,0,0,0)')))
-    fig.add_trace(go.Scatter(x=median_forecast.index, y=median_ci_upper, fill='tonexty', mode='lines', line=dict(color='rgba(255,0,0,0.2)'), name='Median Pricew PSF Prediction Interval'))
+    fig.add_trace(go.Scatter(
+        x=median_monthly_price_psf.index, y=median_monthly_price_psf, mode='lines', name='Median Price PSF'
+    ))
+    fig.add_trace(go.Scatter(
+        x=median_forecast.index, y=median_forecast, mode='lines', name='Forecasted Median'
+    ))
+    fig.add_trace(go.Scatter(
+        x=median_forecast.index, y=median_ci_lower, fill=None, mode='lines', line=dict(color='rgba(255,0,0,0)')
+    ))
+    fig.add_trace(go.Scatter(
+        x=median_forecast.index, y=median_ci_upper, fill='tonexty', mode='lines', line=dict(color='rgba(255,0,0,0.2)'), 
+        name='Median Pricew PSF Prediction Interval'
+    ))
 
     # Update layout
     fig.update_layout(
@@ -52,6 +76,13 @@ def _plot_price_psf_forecast(mean_monthly_price_psf, mean_forecast, mean_ci_lowe
 
 
 def plot_market_overview(in_sample_forecast_length=12, out_sample_forecast_length=12):
-    mean_forecast, mean_ci_lower, mean_ci_upper = _forecast_price_psf(mean_monthly_price_psf, mean_sarimax_best_order, in_sample_forecast_length, out_sample_forecast_length)
-    median_forecast, median_ci_lower, median_ci_upper = _forecast_price_psf(median_monthly_price_psf, median_sarimax_best_order, in_sample_forecast_length, out_sample_forecast_length)
-    return _plot_price_psf_forecast(mean_monthly_price_psf, mean_forecast, mean_ci_lower, mean_ci_upper, median_monthly_price_psf, median_forecast, median_ci_lower, median_ci_upper)
+    mean_forecast, mean_ci_lower, mean_ci_upper = _forecast_price_psf(
+        mean_monthly_price_psf, mean_sarimax_best_order, in_sample_forecast_length, out_sample_forecast_length
+    )
+    median_forecast, median_ci_lower, median_ci_upper = _forecast_price_psf(
+        median_monthly_price_psf, median_sarimax_best_order, in_sample_forecast_length, out_sample_forecast_length
+    )
+    return _plot_price_psf_forecast(
+        mean_monthly_price_psf, mean_forecast, mean_ci_lower, mean_ci_upper, 
+        median_monthly_price_psf, median_forecast, median_ci_lower, median_ci_upper
+    )
